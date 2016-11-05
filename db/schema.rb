6dec588397e513a38ae6dfad1444703059be9b38
tim booher <tim@theboohers.org>
2016-11-05 19:30:45 -0400

    updated with new fields

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161105100117) do
+ActiveRecord::Schema.define(version: 20161105223334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,8 +20,10 @@ ActiveRecord::Schema.define(version: 20161105100117) do
     t.string   "name"
     t.string   "provider"
     t.string   "uid"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.integer  "estimated_retirement_savings"
+    t.integer  "stocks_or_mutual_funds_percent"
   end
 
 end
